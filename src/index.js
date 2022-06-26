a28359a7082fffb96634687a6cacb7a2cbfc6436
Aidana Ualikhan <aidana.ualikhan35@gmail.com>
2022-06-26 18:09:34 +0600

    added ApprovalCard Component, style folder created

--- src/index.js
+++ src/index.js
@@ -3,28 +3,43 @@ import ReactDOM from "react-dom";
 import { faker } from "@faker-js/faker";
 
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import "./style/App.css";
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail
-        author='Sam'
-        date='Today at 6:00 PM'
-        text='Comment 1'
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Alex'
-        date='Today at 7:14 PM'
-        text='Comment 2'
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Jane'
-        date='Yesterday at 2:55 AM'
-        text='Comment 3'
-        image={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <h4>Warning!</h4>
+        <div>Are you really want to do this?</div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author='Sam'
+          date='Today at 6:00 PM'
+          text='Comment 1'
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author='Alex'
+          date='Today at 7:14 PM'
+          text='Comment 2'
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author='Jane'
+          date='Yesterday at 2:55 AM'
+          text='Comment 3'
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
